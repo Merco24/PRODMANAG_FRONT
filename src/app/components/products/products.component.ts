47db7f8662b01fa478b369051abd88d97dfc84ee
Merco <mervicjeazet24@gmail.com>
2024-12-18 23:49:00 +0100

    feat: add getData function productComponent for get all product from api

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {NgForOf} from '@angular/common';
+import {DataService} from '../../service/data.service';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    NgForOf,
+    FormsModule
+  ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit{
+  products: any;
+  constructor(private dataService: DataService) {
+  }
 
+  ngOnInit(): void {
+    this.getProductData();
+  }
+
+  getProductData() {
+    console.log('Liste des produits');
+    this.dataService.getData().subscribe(res=>{
+      console.log(res);
+      this.products = res;
+    })
+  }
+
+  insertData() {
+    console.log('Bonjour insertion test');
+  }
 }
